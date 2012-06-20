09b5929776167678dcbb0281554c545233d8881f
Apechai <matthew.fong@gmail.com>
2012-06-20 01:19:13 -0400

    Drawing a chain with different colors, need to add random colors, gradients, and sideways oriented links

--- Jun21/View.m
+++ Jun21/View.m
@@ -8,6 +8,7 @@
 
 #import "View.h"
 
+
 @implementation View
 
 - (id)initWithFrame:(CGRect)frame
@@ -32,9 +33,9 @@
 {
     // Drawing code
     // Drawing code
-    CGRect bounds = self.bounds;
-    CGFloat radius = .5 * bounds.size.width;	//in pixels
-    
+   CGRect bounds = self.bounds;
+   CGFloat radius = .2 * bounds.size.width;	//in pixels
+     
     /*
      Create the invisible square that will surround the circle.
      Place the upper left corner of the square at the upper left corner of
@@ -42,17 +43,30 @@
      the upper left corner of the View.
      */
     CGRect r = CGRectMake(
-                          bounds.origin.x,
-                          bounds.origin.y,
-                          2 * radius,
-                          2 * radius
+                        bounds.origin.x,
+                        bounds.origin.y,
+                        2 * radius,
+                        2 * radius
                           );
     
-    CGContextRef c = UIGraphicsGetCurrentContext();
-    CGContextBeginPath(c); //unnecessary here: the path is already empty.
-    CGContextAddEllipseInRect(c, r);
-    CGContextSetRGBFillColor(c, 1.0, 0.0, 0.0, 0.1);	//red, opaque
-    CGContextFillPath(c);
+     CGContextRef c = UIGraphicsGetCurrentContext();
+     CGContextBeginPath(c); //unnecessary here: the path is already empty.
+     CGContextAddEllipseInRect(c, r);
+     CGContextSetRGBStrokeColor(c, 0.8, 0.84, 0.84, 0.5);	//tiffany engagement ring silver 206,215,214
+     CGContextStrokePath(c);
+    for (int i = 1; i<=4; i++){
+        CGRect r = CGRectMake(
+                                    bounds.origin.x + radius*i,
+                                    bounds.origin.y + radius*i,
+                                    2 * radius,
+                                    2 * radius
+                                    );
+        CGContextAddEllipseInRect(c, r);
+        CGContextSetRGBStrokeColor(c, 1 - i*0.2, i*0.2, i*0.2, 1);
+        CGContextStrokePath(c);
+        
+    };
+
 }
 
 
